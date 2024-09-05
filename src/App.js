cb07b9e4ef85d2aaf7ebaebe26fd2932f2305563
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-09-04 20:30:17 -0500

    [ADD] Props y atributos - TodoItem / TodoCounter

--- src/App.js
+++ src/App.js
@@ -4,35 +4,33 @@ import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoItem } from "./TodoItem";
+
+const defaultTodos = [
+  { text: "Hacer la tarea de programación", completed: true },
+  { text: "Leer un capítulo del libro de algoritmos", completed: false },
+  { text: "Revisar correos electrónicos", completed: true },
+  { text: "Practicar ejercicios de JavaScript", completed: false },
+  { text: "Limpiar el escritorio de trabajo", completed: true },
+  { text: "Preparar presentación para la reunión del lunes", completed: false },
+  { text: "Actualizar el portafolio en línea", completed: false },
+  { text: "Salir a correr 5 km", completed: true },
+  { text: "Cocinar la cena", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter></TodoCounter>
+    <>
+      <TodoCounter completed={16} total={25}></TodoCounter>
       <TodoSearch></TodoSearch>
 
       <TodoList>
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
-        <TodoItem></TodoItem>
+        {defaultTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}></TodoItem>
+        ))}
       </TodoList>
 
       <CreateTodoButton></CreateTodoButton>
-
-    </div>
-  );
-}
-
-
-
-function TodoItem() {
-  return (
-    <>
-      <li>
-        <span>V</span>
-        <p>Todo Item #1</p>
-        <span>X</span>
-      </li>
     </>
   );
 }
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,8 @@
-function TodoCounter() {
+function TodoCounter({ total, completed }) {
     return (
         <>
             <h1>
-                Has completado 3 de 5 Todos.
+                Has completado {completed} de {total} ToDos.
             </h1>
         </>
     );
